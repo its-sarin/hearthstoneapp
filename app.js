8154c5f1c91b954c854fb5b85504305873cd2733
Tony Recchia <tonyrecchia@me.com>
2014-05-11 22:53:43 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module('app', ['firebase']);
+
+app.service('cardSvc', ['$http', function ($http) {
+    return $http.get('http://hearthstoneapi.herokuapp.com/api/v1/cards.json');
+}]);
+
+app.service('firebaseSvc', ['$firebase', function ($firebase) {
+    var cardRef = new Firebase('https://hearthstoneapp.firebaseio.com/cards');
+
+    return $firebase(cardRef);
+}]);
+
+app.controller('CardCtrl', ['$scope', 'cardSvc', 'firebaseSvc', function ($scope, cardSvc, firebaseSvc) {
+    $scope.selectedIndex = -1;
+
+    $scope.cards = firebaseSvc;
+
+    //cardSvc.success(function (data) {
+    //    $scope.cards = data.cards;
+    //});
+
+    $scope.selectCard = function ($index) {
+        $scope.selectedIndex === $index ? $scope.selectedIndex = -1 : $scope.selectedIndex = $index;
+    }
+}]);
